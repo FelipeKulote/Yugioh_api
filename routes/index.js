2f820356d14ce249a471e7580c3e434be5ab9f08
Felipe Augusto <lipe_augustobq@hotmail.com>
2022-08-11 23:00:47 -0300

    Iniciando o mongodb

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,22 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const cors = require("cors");
-const {
-  showCards,
-  deleteCard,
-  createCard,
-  updateCard,
-} = require("../services/cards.services");
-const {
-  showController,
-  createController,
-  putController,
-  deleteController,
-  searchController,
-} = require("../controllers/cards.controller");
+// const {
+//   showCards,
+//   deleteCard,
+//   createCard,
+//   updateCard,
+// } = require("../services/cards.services");
+const controllerTermos = require("../controllers/cards.controller");
 
 app.use(cors());
-app.get("/", showController);
-app.get("/:id", searchController);
-app.post("/post", createController);
-app.put("/put/:id", putController);
-app.delete("/delete/:id", deleteController);
+app.get("/", controllerTermos.showController);
+app.get("/:id", controllerTermos.searchController);
+app.post("/post", controllerTermos.createController);
+app.put("/put/:id", controllerTermos.putController);
+app.delete("/delete/:id", controllerTermos.deleteController);
+
+
 
 app.listen(3000, () => {
   console.log("http://localhost:3000");
