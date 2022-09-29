c18ee46b837eaec2086eb3175ab064d9d06a0b08
Felipe Augusto <lipe_augustobq@hotmail.com>
2022-09-29 12:06:57 -0300

    feat: implementação do swagger

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const cardsRoutes = require("./routes/cards.route");
+const CardsRoutes = require("./routes/cards.route");
 const { mongoConnect } = require("./database/mongoDb/mongo");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDoc = require("./docs/swagger.json");
@@ -12,10 +12,10 @@ app.use(express.json());
 
 mongoConnect();
 
-app.use("/api/docs", swaggerUi.serve);
-app.get("/api/docs", swaggerUi.setup(swaggerDoc));
-app.use("/cards", cardsRoutes);
+app.use("/api-docs", swaggerUi.serve);
+app.get("/api-docs", swaggerUi.setup(swaggerDoc));
+app.use("/cards", CardsRoutes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT ?? 3000, () => {
   console.log("http://localhost:3000");
 });
